48c8833dad82a8178434f0326bc7d8660e29dbbb
Zack <zack.musicteacher@gamil.com>
2020-07-06 14:33:47 -0700

    create widgets app

---
+++ widgets/src/clock.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+export const Clock = props => {
+    const [time, setTime] = useState(new Date())
+    useEffect(() => {
+        
+    })
+    return(
+        <div>
+            <h1>Clock</h1>
+
+        </div>
+    )
+}
