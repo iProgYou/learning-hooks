33953df3d9d34f2a2fcd47bae8d6942ee01b6799
Zack <zack.musicteacher@gamil.com>
2020-07-06 16:29:14 -0700

    start autocomplete

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Clock from './components/clock'
 import Tabs from './components/tabs';
 import Weather from './components/weather';
+import Autocomplete from './components/autocomplete';
 
 function App() {
   let tabs = [
@@ -10,11 +11,15 @@ function App() {
     {title:'tab 2',content: 'Whats going on here'},
     {title:'tab 3',content: 'blugh'}
   ]
+  let names = [
+
+  ]
   return (
     <div className="App">
       <Clock />
       <Tabs tabs={tabs}/>
       <Weather />
+      <Autocomplete names={names} />
     </div>
   );
 }
---
+++ widgets/src/components/autocomplete.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Autocomplete() {
+    return null;
+}
