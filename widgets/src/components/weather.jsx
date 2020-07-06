d5865961e7ee223ae3fc1fedd7f5f87791d6adc2
Zack <zack.musicteacher@gamil.com>
2020-07-06 16:14:45 -0700

    weather done

--- widgets/src/components/weather.jsx
+++ widgets/src/components/weather.jsx
@@ -5,21 +5,23 @@ import React, { useState, useEffect } from 'react';
 export default function Weather() {
     const [weather,setWeather] = useState(null)
     useEffect(() => {
-        // let proxy = "https://cors-anywhere.herokuapp.com/"
-        // let weather = "https://samples.openweathermap.org/data/2.5/weather?zip=94607,us&appid=439d4b804bc8187953eb36d2a8c26a02"
-        // fetch(proxy + weather)
-        //     .then(payload => payload.json())
-        //     .then(payload => {
-        //         console.log(payload)
-        //         setWeather(payload)
-        //     })
-    })
+        let proxy = "https://cors-anywhere.herokuapp.com/"
+        let weather = "https://samples.openweathermap.org/data/2.5/weather?zip=94607,us&appid=439d4b804bc8187953eb36d2a8c26a02"
+        fetch(proxy + weather)
+            .then(payload => payload.json())
+            .then(payload => {
+                console.log(payload)
+                setWeather(payload)
+            })
+    },[])
     
     if (!weather) return null;
     return(
         <div>
             <h1>Here is the weather in {weather.name}</h1>
-            <h3></h3>
+            <h3>Temp: {weather.main.temp}</h3>
+            <h3>Humidity: {weather.main.humidity}</h3>
+            <h3>Weather: {weather.weather[0].main}</h3>
         </div>
     )
 }
