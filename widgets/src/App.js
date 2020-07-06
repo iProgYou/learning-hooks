bac511e2d69a4292e641a6bae3df0a1636be9af2
Zack <zack.musicteacher@gamil.com>
2020-07-06 14:55:38 -0700

    clock widget done

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Clock from './clock'
+import Tabs from './tabs';
 
 function App() {
   return (
     <div className="App">
-      <Clock
-
-      />
+      <Clock />
+      <Tabs />
     </div>
   );
 }
--- widgets/src/clock.jsx
+++ widgets/src/clock.jsx
@@ -1,14 +1,29 @@
 import React, { useState, useEffect } from 'react';
 
-export const Clock = props => {
+export default function Clock() {
     const [time, setTime] = useState(new Date())
+    // let intervalId = null;
     useEffect(() => {
-        
+        let intervalId = setInterval(() => {
+            setTime(new Date())
+        }, 1000)
+        return () => {
+            clearInterval(intervalId)
+        }
     })
+
+    function getDate() {
+        return time.toDateString()
+    }
+
+    function getTime() {
+        return time.getTime()
+    }
     return(
         <div>
             <h1>Clock</h1>
-
+            <h3>{getDate()}</h3>
+            <h3>{getTime()}</h3>
         </div>
     )
-}
+};
